43c930009c56f91aff7cf97a22530d9ba6f863e5
jharreldesign <harrel.jason@gmail.com>
2024-11-14 11:19:32 -0800

    ability to get posts

---
+++ controllers/hoots.js
@@ -0,0 +1,32 @@
+const express = require(`express`);
+const verifyToken = require(`../middleware/verify-token.js`);
+const Hoot = require(`../models/hoot.js`);
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+  try {
+    const hoots = await Hoot.find({})
+      .populate(`author`)
+      .sort({ createdAt: `desc` });
+    res.status(200).json(hoots);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.use(verifyToken);
+
+router.post(`/`, async (req, res) => {
+  try {
+    req.body.author = req.user._id;
+    const hoot = await Hoot.create(req.body);
+    hoot._doc.author = req.user;
+    res.status(201).json(hoot);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+
+module.exports = router;
